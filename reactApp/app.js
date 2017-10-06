92fe3cdc8a15208666f7f40ff2981a9519864e5e
nikitabondarenko <nob2107@columbia.edu>
2017-10-06 16:26:52 -0700

    git git git

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,6 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const dummyData = ['eat', 'sleep', 'code', 'repeat']
+
+class InputLine extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div>
+        <input type = "text"/>
+        <input type = "submit" value = "Submit" />
+      </div>
+    )
+  }
+}
 
 
-ReactDOM.render(<p>hello ay</p>,
-   document.getElementById('root'));
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return(<ul> {dummyData.map((dummyData) => (
+      <li>
+        <button> X </button>
+        {dummyData}
+      </li>))}
+      </ul>
+    )
+  }
+}
+
+class TodoApp extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div>
+      <InputLine />
+      <TodoList />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
